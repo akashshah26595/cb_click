5be138049d660c94e798bdbd8c0f663f413d91e3
Akash Shah <akashshah26595@gmail.com>
2016-07-31 14:28:17 +0530

    Init Commit. Create Snapshot working properly, taking name as input from user

---
+++ cb.py
@@ -0,0 +1,70 @@
+import click
+import urllib2
+import urllib
+from urlparse import urlparse
+import json
+
+class Config(object):
+	def __init__(self):
+		pass
+
+pass_config = click.make_pass_decorator(Config,ensure=True)
+@click.group()
+@click.argument('cinder_id',default='77b26fc7-066e-3057-b131-e77b4f6835cc')
+@click.argument('cb_command')
+@pass_config
+
+def display(config,cinder_id,cb_command):
+	key_file = open('cbkey.txt','r')
+	url_file = open('cburl.txt','r')
+	res_file = open('cbres.txt','r')
+	click.echo('Hello World')
+	key = key_file.read()
+	key = key.strip()
+	url = url_file.read()
+	url = url.strip()
+	res = res_file.read()
+	if(cb_command =='createStorageSnapshot'):
+		name = raw_input('Enter snapshot name:\n')
+		config.name = name
+	config.cinder_id = cinder_id
+	config.cb_command = cb_command
+	config.key=key;
+	config.url=url;
+	config.res=res;
+
+
+@display.command()
+@pass_config	
+def createSnapshot(config):
+	"""This method creates a snapshot for provided cinder id"""
+	#https://20.10.43.1/client/api?command=createStorageSnapshot&
+	#id=77b26fc7-066e-3057-b131-e77b4f6835cc&name=snap2&response=json
+	urlData = config.url + "apiKey=" + config.key + "&command=" + config.cb_command + "&id=" + config.cinder_id  + "&name=" + config.name +"&response=" + config.res 
+	click.echo(urlData)
+	webUrl = urllib2.urlopen(urlData)
+	click.echo(webUrl.getcode())
+	click.echo(urlData)
+	if(webUrl.getcode() == 200):
+		data = webUrl.read()
+		#print data
+		click.echo((data))
+	else:
+		click.echo('Error while fetching data', str(webUrl.getcode()))
+'''
+@display.command()
+@pass_config
+def viewSnapshots(config):
+	urlData = config.url + "apiKey=" + config.key + "&command=" + config.cb_command + "&id=" + config.cinder_id  + "&name=" + config.name +"&response=" + config.res 
+	click.echo(urlData)
+	webUrl = urllib2.urlopen(urlData)
+	click.echo(webUrl.getcode())
+	click.echo(urlData)
+	if(webUrl.getcode() == 200):
+		data = webUrl.read()
+		#print data
+		click.echo((data))
+	else:
+		click.echo('Error while fetching data', str(webUrl.getcode()))	
+
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+name='CloudByte',
+version='1.0',
+py_modules=['cb'],
+install_requires=[
+	'Click',
+],
+entry_points='''
+	[console_scripts]
+	cb=cb:display
+''',
+)
+
