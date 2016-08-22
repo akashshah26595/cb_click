ccbfcedad0bcf81717ea9a33f4d28b9207971543
Akash Shah <akashshah26595@gmail.com>
2016-08-22 11:14:53 +0530

    Minor changes to virsh_instance.py

--- virsh_instance.py
+++ virsh_instance.py
@@ -1,7 +1,7 @@
 import subprocess
 import shlex
 
-p = subprocess.Popen("./virsh_check.sh | awk ' $2!=\"Name\" {print $2}'",stdout=subprocess.PIPE,shell=True)
+p = subprocess.Popen("./virsh_check.sh | awk ' $2!="Name" {print $2}'",stdout=subprocess.PIPE,shell=True)
 (out,err) = p.communicate()
 p_status = p.wait()
 #tmp = proc.stdout.read()
